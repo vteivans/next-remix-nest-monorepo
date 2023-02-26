66804429216ab8bdb71fac9fe5289c48802faaf5
Viesturs Teivāns <vteivans@gmail.com>
2023-02-26 19:05:04 +0200

    id routes - route that's dynamic and can get the parameter from path

---
+++ apps/remix-fe/app/routes/jokes/$jokeId.tsx
@@ -0,0 +1,11 @@
+export default function Route() {
+  return (
+    <div>
+      <p>Here&apos;s your hilarious joke:</p>
+      <p>
+        Why don&apos;t you find hippopotamuses hiding in trees? They&apos;re
+        really good at it.
+      </p>
+    </div>
+  );
+}
